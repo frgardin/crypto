4f19877737c1c96f9b30d3434e5556c3be5490c4
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2026-01-12 10:37:12 -0300

    add api prefix in decrypt endpoint

--- src/main/java/org/gardin/felipe/crypto/ResourcePath.java
+++ src/main/java/org/gardin/felipe/crypto/ResourcePath.java
@@ -5,7 +5,7 @@ public final class ResourcePath {
     private static final String API = "/api";
 
 
-    public static final String DECRYPT_PATH = "/decrypt";
+    public static final String DECRYPT_PATH = API + "/decrypt";
     public static final String ENCRYPT_PATH = API + "/encrypt";
 
     public ResourcePath() throws IllegalAccessException {
